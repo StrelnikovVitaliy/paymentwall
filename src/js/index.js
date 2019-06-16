ced246fba4c5dcb98eb84f78450e9c3144fe7b17
vitalii_strelnikov <vitalii_s@codesushi.co>
2019-06-16 14:07:57 +0300

    fixed semantic mistakes

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,3 @@
-// function openExample(evt, languageInit, languageStore) {
-//     var i, examples, tablinks;
-//     examples = document.getElementsByClassName("examples");
-//     for (i = 0; i < examples.length; i++) {
-//         examples[i].style.display = "none";
-//     }
+// import './tabs/tabs.js';
 
-//     tablinks = document.getElementsByClassName("tablinks");
-//     for (i = 0; i < tablinks.length; i++) {
-//         tablinks[i].className = tablinks[i].className.replace(" active", "");
-//     }
 
-//     document.getElementById(languageInit).style.display = "block";
-//     document.getElementById(languageStore).style.display = "block";
-//     evt.currentTarget.className += " active";
-// }
